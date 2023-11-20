d8c5263155fc97d90dfc1544594f91e960ca84eb
PePeKuBk0 <jakub.venetianer@gmail.com>
2023-11-20 13:15:32 +0100

    idk

---
+++ Zeek/src/Hra.java
@@ -0,0 +1,7 @@
+public class Hra {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("n-word");
+        }
+    }
